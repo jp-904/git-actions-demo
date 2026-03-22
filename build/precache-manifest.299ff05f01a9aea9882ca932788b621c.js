a5f0603b9b1c325d251a600b0eea1e503f5b0f15
AlaCarte413 <ala.carte413@gmail.com>
2026-03-22 11:27:12 +1100

    init

---
+++ build/precache-manifest.299ff05f01a9aea9882ca932788b621c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc180afb3592423af42a48ca2f92843",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "447c11c73dc7ade50a70",
+    "url": "/To-Do-App-in-React/static/css/main.f171ecbb.chunk.css"
+  },
+  {
+    "revision": "5ec13f1041afb1ed426e",
+    "url": "/To-Do-App-in-React/static/js/2.0c50773d.chunk.js"
+  },
+  {
+    "revision": "447c11c73dc7ade50a70",
+    "url": "/To-Do-App-in-React/static/js/main.b66cb159.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
